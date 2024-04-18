08fe14190965821507580c392d7692dccf3efd50
syaifulsmk10 <syaifulnazar47@gmail.com>
2024-04-18 12:09:18 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -32,9 +32,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // // /users
 Route::get('/users', function (Request $request) {
     // Ambil data dari JSONPlaceholder
-    $users = Http::get('https://jsonplaceholder.typicode.com/users')->json();
+    $users = Http::get('https://jsonplaceholder.typicode.com/posts')->json();
     
-    // Filter berdasarkan userId jika disediakan
+ 
     $userId = $request->query('userId');
     if ($userId) {
         $users = array_filter($users, function($user) use ($userId) {
@@ -45,26 +45,12 @@ Route::get('/users', function (Request $request) {
     return response()->json($users);
 });
 
-Route::get('/users/{id}', function(Request $request, $id){
-    $response = Http::get('https://jsonplaceholder.typicode.com/users');
-    $users = $response->json();
-    
-    // Filter data berdasarkan userId jika parameter query disediakan
-    
-        $users_filter = array_filter($users, function($user) use ($id) {
-            return $user['id'] == $id;
-        });
-    
-    
-    return response()->json($users_filter);
-});
 
 // /todos
 Route::get('/todos', function (Request $request) {
-    // Ambil data dari JSONPlaceholder
     $todos = Http::get('https://jsonplaceholder.typicode.com/todos')->json();
     
-    // Filter berdasarkan userId jika disediakan
+ 
     $userId = $request->query('userId');
     if ($userId) {
         $todos = array_filter($todos, function($todo) use ($userId) {
@@ -72,7 +58,6 @@ Route::get('/todos', function (Request $request) {
         });
     }
 
-    // Filter todos berdasarkan status completed jika disediakan
     $completed = $request->query('completed');
     if ($completed !== null) {
         $todos = array_filter($todos, function($todo) use ($completed) {
@@ -140,46 +125,117 @@ Route::get('/posts', function (Request $request) {
 });
 
 
+Route::get('/users/{id}', function(Request $request, $id){
+    $response = Http::get('https://jsonplaceholder.typicode.com/users');
+    $users = $response->json();
+    
+    
+        $users_filter = array_filter($users, function($user) use ($id) {
+            return $user['id'] == $id;
+        });
+    
+    
+    return response()->json($users_filter);
+});
+
+Route::get('/todos/{id}', function ($id) {
+    $todos = Http::get('https://jsonplaceholder.typicode.com/todos')->json();
+
+    $todos_filter = array_filter($todos, function ($todo) use ($id) {
+
+        return $todo["id"] == $id;
+    });
+
+    return response()->json($todos_filter);
+});
+
+
+Route::get('/albums/{id}', function ($id) {
+    $albums  = Http::get('https://jsonplaceholder.typicode.com/albums')->json();
+
+    $albums_filter = array_filter($albums, function($album) use ($id){
+        return $album['id'] == $id;
+    });
+
+    return response()->json($albums_filter);
+});
+
+
+Route::get('/posts/{id}', function ($id) { 
+    $posts = Http::get('https://jsonplaceholder.typicode.com/posts')->json();
+
+    $posts_filter = array_filter($posts, function ($post) use ($id) {
+        return  $post['id'] == $id;
+    });
+
+    return response()->json($posts_filter);
+
+});
+
+
+Route::get('/photos/{id}', function ($id) { 
+    $photos = Http::get('https://jsonplaceholder.typicode.com/photos')->json();
+
+    $photos_filter = array_filter($photos, function ($photo) use ($id) {
+       return   $photo['id'] == $id;
+    });
+
+    return response()->json($photos_filter);
+
+});
+
+
+Route::get('/comments/{id}', function ($id) { 
+    $comments = Http::get('https://jsonplaceholder.typicode.com/comments')->json();
+
+    $comments_filter = array_filter($comments, function ($comment) use ($id) {
+       return  $comment['id'] == $id;
+    });
+
+    return response()->json($comments_filter);
+
+});
+
+
+
+
+
+
+
 
 //get data dengan id dan parameter
 
 Route::get('/users/{id}/{param}', function (Request $request, $id, $param) {
-    // Ambil data dari JSONPlaceholder
     $posts = Http::get('https://jsonplaceholder.typicode.com/posts')->json();
     $comments = Http::get('https://jsonplaceholder.typicode.com/comments')->json();
     $albums = Http::get('https://jsonplaceholder.typicode.com/albums')->json();
     $photos = Http::get('https://jsonplaceholder.typicode.com/photos')->json();
     $todos = Http::get('https://jsonplaceholder.typicode.com/todos')->json();
 
-    // variabel untuk menyimpan hasil filter
     $result = [];
-
-    //menentukan berdasarkan parameter
     switch ($param) {
         case 'posts':
-            // Filter posts berdasarkan userId
+
             $result = array_filter($posts, function ($post) use ($id) {
                 return $post['userId'] == $id;
             });
             break;
         
         case 'comments':
-            // Filter comments berdasarkan userId
+
             
             $result = array_filter($comments, function ($comment) use ($id) {
-                return $comment['postId'] == $id; // Anda dapat menggunakan logika tambahan untuk memetakan postId ke userId
+                return $comment['postId'] == $id; 
             });
             break;
 
         case 'albums':
-            // Filter albums berdasarkan userId
             $result = array_filter($albums, function ($album) use ($id) {
                 return $album['userId'] == $id;
             });
             break;
             
         case 'photos':
-            // Filter photos berdasarkan albumId (Anda perlu memetakan userId ke albumId)
             $userAlbums = array_filter($albums, function ($album) use ($id) {
                 return $album['userId'] == $id;
             });
@@ -193,89 +249,93 @@ Route::get('/users/{id}/{param}', function (Request $request, $id, $param) {
             break;
         
         case 'todos':
-            // Filter todos berdasarkan userId
             $result = array_filter($todos, function ($todo) use ($id) {
                 return $todo['userId'] == $id;
             });
+
+             $completed = $request->query('completed');
+                if ($completed !== null) {
+                     $todos = array_filter($todos, function($todo) use ($completed) {
+                 return $todo['completed'] == ($completed === 'true');
+                 });
+             }
+                return response()->json($todos);
+            
             break;
 
         default:
-            // Kembalikan respon 404 jika param tidak valid
-            return response()->json(['error' => 'Invalid parameter'], 404);
+            return response()->json(['error' => 'Invalid parameter'], 404);          
     }
-
-    // Kembalikan hasil dalam format JSON
     return response()->json($result);
 });
 
-Route::get('/users/{userId}/comments', function ($userId) {
-    // Ambil data dari JSONPlaceholder API
-    // Ambil semua postingan pengguna berdasarkan user_id
-    $postsResponse = Http::get('https://jsonplaceholder.typicode.com/posts', [
-        'userId' => $userId
-    ]);
+
+
+// Route::get('/users/{userId}/comments', function ($userId) {
+//     // Ambil data dari JSONPlaceholder API
+//     // Ambil semua postingan pengguna berdasarkan user_id
+//     $postsResponse = Http::get('https://jsonplaceholder.typicode.com/posts', [
+//         'userId' => $userId
+//     ]);
     
-    // Periksa apakah permintaan berhasil
-    if ($postsResponse->successful()) {
-        $posts = $postsResponse->json();
+//     // Periksa apakah permintaan berhasil
+//     if ($postsResponse->successful()) {
+//         $posts = $postsResponse->json();
 
-        // Ambil semua komentar yang terkait dengan postId yang didapat
-        $postIds = collect($posts)->pluck('id');
+//         // Ambil semua komentar yang terkait dengan postId yang didapat
+//         $postIds = collect($posts)->pluck('id');
 
-        // Lakukan permintaan ke JSONPlaceholder API untuk mengambil data komentar
-        $commentsResponse = Http::get('https://jsonplaceholder.typicode.com/comments', [
-            'postId' => $postIds->toArray()
-        ]);
+//         // Lakukan permintaan ke JSONPlaceholder API untuk mengambil data komentar
+//         $commentsResponse = Http::get('https://jsonplaceholder.typicode.com/comments', [
+//             'postId' => $postIds->toArray()
+//         ]);
 
-        // Periksa apakah permintaan berhasil
-        if ($commentsResponse->successful()) {
-            $comments = $commentsResponse->json();
+//         // Periksa apakah permintaan berhasil
+//         if ($commentsResponse->successful()) {
+//             $comments = $commentsResponse->json();
 
-            // Kembalikan data komentar dalam format JSON
-            return Response()->json($comments);
-        } else {
-            return Response()->json(['error' => 'Failed to fetch comments'], $commentsResponse->status());
-        }
-    } else {
-        return Response()->json(['error' => 'Failed to fetch posts'], $postsResponse->status());
-    }
-});
+//             // Kembalikan data komentar dalam format JSON
+//             return Response()->json($comments);
+//         } else {
+//             return Response()->json(['error' => 'Failed to fetch comments'], $commentsResponse->status());
+//         }
+//     } else {
+//         return Response()->json(['error' => 'Failed to fetch posts'], $postsResponse->status());
+//     }
+// });
 
 
-Route::get('/users/{userId}/photos', function ($userId) {
-    // Ambil data dari JSONPlaceholder API
-    // Ambil semua postingan pengguna berdasarkan user_id
-    $albumsResponse = Http::get('https://jsonplaceholder.typicode.com/albums', [
-        'userId' => $userId
-    ]);
+// Route::get('/users/{userId}/photos', function ($userId) {
+//     // Ambil semua postingan pengguna berdasarkan user_id
+//     $albumsResponse = Http::get('https://jsonplaceholder.typicode.com/albums', [
+//         'userId' => $userId
+//     ]);
     
-    // Periksa apakah permintaan berhasil
-    if ($albumsResponse->successful()) {
-        $albums = $albumsResponse->json();
+//     // Periksa apakah permintaan berhasil
+//     if ($albumsResponse->successful()) {
+//         $albums = $albumsResponse->json();
 
-        // Ambil semua komentar yang terkait dengan postId yang didapat
-        $postIds = collect($albums)->pluck('id');
+//         // Ambil semua komentar yang terkait dengan postId yang didapat
+//         $postIds = collect($albums)->pluck('id');
 
-        // Lakukan permintaan ke JSONPlaceholder API untuk mengambil data komentar
-        $photosResponse = Http::get('https://jsonplaceholder.typicode.com/photos', [
-            'postId' => $postIds->toArray()
-        ]);
+//         // Lakukan permintaan ke JSONPlaceholder API untuk mengambil data komentar
+//         $photosResponse = Http::get('https://jsonplaceholder.typicode.com/photos', [
+//             'postId' => $postIds->toArray()
+//         ]);
 
-        // Periksa apakah permintaan berhasil
-        if ($photosResponse->successful()) {
-            $photos = $photosResponse->json();
+//         // Periksa apakah permintaan berhasil
+//         if ($photosResponse->successful()) {
+//             $photos = $photosResponse->json();
 
-            // Kembalikan data komentar dalam format JSON
-            return Response()->json($photos);
-        } else {
-            return Response()->json(['error' => 'Failed to fetch comments'], $photosResponse->status());
-        }
-    } else {
-        return Response()->json(['error' => 'Failed to fetch posts'], $albumsResponse->status());
-    }
-});
-
-
+//             // Kembalikan data komentar dalam format JSON
+//             return Response()->json($photos);
+//         } else {
+//             return Response()->json(['error' => 'Failed to fetch comments'], $photosResponse->status());
+//         }
+//     } else {
+//         return Response()->json(['error' => 'Failed to fetch posts'], $albumsResponse->status());
+//     }
+// });
 
 
 
@@ -295,10 +355,12 @@ Route::post("/posttt", function (Request $request) {
         'body' => $body,
     ]);
 
-
-    return response()->json([
+    if ($reqsponse->successful()) {
+       return response()->json([
         'message' => "berhasil post data"
-    ]);
+    ]); 
+    }
+    
 });
 
 
